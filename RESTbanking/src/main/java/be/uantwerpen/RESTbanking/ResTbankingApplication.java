67b8b720f50971ea91fd5aa065a8972c8cb8c27b
asifwasefi <asifwasefi@yahoo.com>
2021-04-27 01:19:13 +0200

    Blank project
    Blank project downloaded from  https://start.spring.io/ with only dependency "REST repository" and MAVEN
    
    This project was then imported

---
+++ RESTbanking/src/main/java/be/uantwerpen/RESTbanking/ResTbankingApplication.java
@@ -0,0 +1,13 @@
+package be.uantwerpen.RESTbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTbankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTbankingApplication.class, args);
+	}
+
+}
---
+++ RESTbanking/src/test/java/be/uantwerpen/RESTbanking/ResTbankingApplicationTests.java
@@ -0,0 +1,13 @@
+package be.uantwerpen.RESTbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTbankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
